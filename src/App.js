4f53f4aade7574e1ee83d3d87949136af43e9a7b
ram05kishor12 <ramkishore29861@gmail.com>
2023-11-13 22:18:25 +0530

    select option fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const [selectedVoice, setSelectedVoice] = useState(null);
+  const [voices, setVoices] = useState([]);
+  const [text, setText] = useState('');
 
-  const handleConvert = () => {
-    const text = document.getElementById('text').value;
+  useEffect(() => {
+    const updateVoices = () => {
+      const voices = window.speechSynthesis.getVoices();
+      setVoices(voices);
+      setSelectedVoice(voices.find((voice) => voice.default));
+    };
+
+    updateVoices(); // Initial update
+    window.speechSynthesis.onvoiceschanged = updateVoices; // Update when voices change
+
+    // Cleanup event listener when the component unmounts
+    return () => {
+      window.speechSynthesis.onvoiceschanged = null;
+    };
+  }, []);
+
+  const handlePlay = () => {
     if (!text) {
       alert('Please enter text to convert.');
       return;
@@ -18,7 +35,6 @@ function App() {
   };
 
   const handleDownload = () => {
-    const text = document.getElementById('text').value;
     if (!text) {
       alert('Please enter text to convert.');
       return;
@@ -27,7 +43,7 @@ function App() {
     const utterance = new SpeechSynthesisUtterance(text);
     utterance.voice = selectedVoice;
 
-    const blob = new Blob([new Uint8Array([])], { type: 'audio/wav' });
+    const blob = new Blob([new Uint8Array(new TextEncoder().encode(text))], { type: 'audio/wav' });
     const url = URL.createObjectURL(blob);
     const a = document.createElement('a');
     a.href = url;
@@ -38,7 +54,6 @@ function App() {
 
   const handleVoiceChange = (event) => {
     const selectedVoiceURI = event.target.value;
-    const voices = window.speechSynthesis.getVoices();
     const selectedVoice = voices.find((voice) => voice.voiceURI === selectedVoiceURI);
     setSelectedVoice(selectedVoice);
   };
@@ -50,18 +65,23 @@ function App() {
           <h1>Text-Speech Converter</h1>
         </div>
         <div className="box">
-          <textarea id="text" placeholder="Enter your text here..."></textarea>
+          <textarea
+            id="text"
+            placeholder="Enter your text here..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          ></textarea>
         </div>
         <div className="row">
           <select onChange={handleVoiceChange}>
-            {window.speechSynthesis.getVoices().map((voice) => (
+            {voices.map((voice) => (
               <option key={voice.voiceURI} value={voice.voiceURI}>
                 {voice.name}
               </option>
             ))}
           </select>
-          <button id="convertButton" onClick={handleConvert}>
-            Convert
+          <button id="convertButton" onClick={handlePlay}>
+            Play
           </button>
           <button id="downloadButton" onClick={handleDownload}>
             Download
